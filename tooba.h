81353fb7040a1159a83cbfb2d6b3117dccc5c29e
mohamedalzahaby <31936040+mohamedalzahaby@users.noreply.github.com>
2018-10-21 18:05:48 +0200

    Add files via upload

---
+++ tooba.h
@@ -0,0 +1,18 @@
+#pragma once
+class tooba
+{
+private:
+	int x, y;
+	int length, width;
+	int RGB1, RGB2,  RGB3;
+
+public:
+	tooba();
+	tooba(int , int ,int ,int , int ,int ,int);
+	void setCoordinates(int ,int );
+	void setColor(int, int, int);
+	void getColor();
+	void drawMyBrick(int, int, int, int);
+	
+};
+
